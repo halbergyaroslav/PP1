c7dae40510aecdf2e0c96f8a3249a87b39059d20
halbergyaroslav <109332633+halbergyaroslav@users.noreply.github.com>
2023-01-23 19:39:33 +0600

    new

---
+++ Lab1/D.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+    int a;
+    double b;
+    cin >> a;
+    b = sqrt(a);
+    cout << setprecision(11) << b;
+}
