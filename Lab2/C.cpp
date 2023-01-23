b2c9ad20aed4bc15186c03577cb1d9beea200f28
halbergyaroslav <109332633+halbergyaroslav@users.noreply.github.com>
2023-01-23 19:44:16 +0600

    new

---
+++ Lab2/C.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 4 != 0) cout << "NO";
+    else if (n % 100 != 0) cout << "YES";
+    else if (n % 400 != 0) cout << "NO";
+    else cout << "YES";
+}
