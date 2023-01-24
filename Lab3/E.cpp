4eaaf59e74ebe88fdf4b272c29be3dfd81f9f72c
halbergyaroslav <109332633+halbergyaroslav@users.noreply.github.com>
2023-01-24 10:34:11 +0600

    new

---
+++ Lab3/E.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long long int sum = 0;
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++) cin >> array[i];
+    for (int i = 0; i < n; i++) sum += array[i];
+    cout << sum;
+}
