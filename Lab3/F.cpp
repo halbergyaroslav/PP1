39f6fff8e132d829c6efff0333a0f737bcbf0cab
halbergyaroslav <109332633+halbergyaroslav@users.noreply.github.com>
2023-01-24 10:34:34 +0600

    new

---
+++ Lab3/F.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++) cin >> array[i];
+    for (int i = n - 1; i > -1; i--) cout << array[i] << ' ';
+}
